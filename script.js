b8a1ee950f9126c0e63f8e78bb51958edf168e5e
Tekie79 <tekiefilm79@gmail.com>
2021-11-01 16:08:54 -0400

    Style and Fetch Data

---
+++ script.js
@@ -0,0 +1,64 @@
+const textInput = document.querySelector(".search-bar__input");
+const searchBtn = document.querySelector(".search-icon");
+const searchResultText = document.querySelector(".search-result__text");
+const searchResultContainer = document.querySelector(
+  ".search-result__container"
+);
+
+// fetchJsonp('http://localhost:3000').then(res => res.json()).then(json => console.log(json));
+
+// Fetch search result//
+
+const fetchSearch = async (artistName) => {
+  try {
+    const response =
+      await fetch(`https://itunes.apple.com/search?term=${artistName}&media=music&entity=album&attribute=artistTerm&limit=100
+  `);
+    if (!response.ok) {
+      throw new Error(`Bad request: ${result.status}, ${result.text}`);
+    } else {
+      const data = await response.json();
+      const result = data.results;
+      console.log(data);
+      //Search Result Info
+      const resultInfo = `${data.resultCount} results for "${textInput.value}" `;
+      searchResultText.innerHTML = resultInfo;
+
+      // MAP THE RESULT
+
+      const renderAlbum =
+        result &&
+        result.map((album) => {
+          return ` <div class="result-card">
+          <div class="card-media">
+            <img class="card-picture" src=${album.artworkUrl60} alt="artist picture" />
+          </div>
+          <div class="card-title">
+            <p class="card-album__title">${album.collectionName}</p>
+          </div>
+        </div>
+          `;
+        });
+
+      // Render to the DOM
+      searchResultContainer.innerHTML = renderAlbum.join("");
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// Event Handlers
+
+searchBtn.addEventListener("click", () => {
+  const searchName = textInput.value;
+  fetchSearch(searchName);
+});
+
+document.addEventListener("keydown", (event) => {
+  const keyName = event.key;
+
+  if (keyName === "Enter") {
+    fetchSearch(textInput.value);
+  }
+});
