5f84ba20607a43ef30805b3fa7cd73294697488c
Tekie79 <tekiefilm79@gmail.com>
2021-11-03 13:27:05 -0400

    Modal and Debounce

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const searchResultText = document.querySelector(".search-result__text");
 const searchResultContainer = document.querySelector(
   ".search-result__container"
 );
-
+const modalContainer = document.querySelector(".modal-container");
 // Fetch search result//
 
 const fetchSearch = async (artistName) => {
@@ -27,7 +27,7 @@ const fetchSearch = async (artistName) => {
       const renderAlbum =
         result &&
         result.map((album) => {
-          return ` <div class="result-card">
+          return ` <div class="result-card" onclick="toggleModal(${album.collectionId})">
           <div class="card-media">
             <img class="card-picture" src=${album.artworkUrl60} alt="artist picture" />
           </div>
@@ -55,12 +55,56 @@ searchBtn.addEventListener("click", () => {
   }
 });
 
-document.addEventListener("keydown", (event) => {
-  const keyName = event.key;
+// Debounce Method
+const debounce = function (fn, delay) {
+  let timer;
+  return function (...args) {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      fn.apply(this, args);
+    }, delay);
+  };
+};
 
-  if (keyName === "Enter") {
-    if (textInput.value) {
-      fetchSearch(textInput.value);
-    }
-  }
-});
+textInput.addEventListener(
+  "keyup",
+  debounce(() => {
+    fetchSearch(textInput.value);
+  }, 500)
+);
+
+// Modal optional
+
+const toggleModal = async (id) => {
+  const responseData = await fetch(
+    `https://itunes.apple.com/lookup?id=${id}&entity=song`
+  );
+  const albumData = await responseData.json();
+  const albumInfo = albumData.results[0];
+  console.log(albumInfo);
+  const modalComponent = ` <div class="album-details__modal">
+  <i onclick="onModalClose()" class="modal-close fas fa-close"></i>
+  <div class="details-modal__media">
+    <img class="modal-image" src=${albumInfo.artworkUrl100} />
+  </div>
+  <div class="details-modal__body">
+    <p class="modal-artist_name">${albumInfo.artistName}</p>
+    <h2 class="modal-album_title">${albumInfo.collectionName}</h2>
+    <p class="modal-genre">Genre: ${albumInfo.primaryGenreName}</p>
+    <p class="modal-release__date">Released: ${new Date(
+      albumInfo.releaseDate
+    ).toLocaleDateString()}</p>
+    <p class="modal-label">${albumInfo.copyright}s</p>
+  </div>
+</div>`;
+  // render to DOM
+
+  modalContainer.classList.remove("hidden");
+  modalContainer.innerHTML = modalComponent;
+};
+
+// Close Modal
+
+const onModalClose = () => {
+  modalContainer.classList.add("hidden");
+};
